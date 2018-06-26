1f5e0ef95100b0b5193c460c234bf14167e411b5
shakediel <shaked921@gmail.com>
2018-06-27 01:12:46 +0300

    sys args

--- ‫‪myexecuter‬‬.py
+++ ‫‪myexecuter‬‬.py
@@ -5,8 +5,8 @@ from pddlsim.planner import local
 
 from StochasticSmartReplanner import StochasticSmartReplanner
 
-domain_path = "domain.pddl"
-problem_path = "t_5_5_5_multiple.pddl"
+# domain_path = "domain.pddl"
+# problem_path = "t_5_5_5_multiple.pddl"
 # problem_path = "ahinoam_problem.pddl"
 # problem_path = "failing_actions_example.pddl"
 # domain_path = "freecell_domain.pddl"
@@ -15,7 +15,7 @@ problem_path = "t_5_5_5_multiple.pddl"
 # problem_path = "rover_problem.pddl"
 # domain_path = "satellite_domain.pddl"
 # problem_path = "satellite_problem.pddl"
-# domain_path = sys.argv[1]
-# problem_path = sys.argv[2]
+domain_path = sys.argv[1]
+problem_path = sys.argv[2]
 print(LocalSimulator(local).run(domain_path, problem_path,
                                 StochasticSmartReplanner(problem_path)))
